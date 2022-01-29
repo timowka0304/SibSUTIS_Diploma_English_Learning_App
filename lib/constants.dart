d0c844e556beba10aa6b66126dc593f73c45461c
timowka0304 <timoti20@mail.ru>
2022-01-29 16:33:33 +0700

    Start config

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const kWhite = Color(0xFFFFFFFF);
+const kMainPurple = Color(0xFF595ABA);
+const kMainPink = Color(0xFFED6078);
+const kMainTextColor = Color(0xFF414141);
+
+const String k1stImageOnboardingPath = 'assets/images/onboarding/1st_img.png';
+const String k2ndImageOnboardingPath = 'assets/images/onboarding/2nd_img.png';
+const String k3rdImageOnboardingPath = 'assets/images/onboarding/3rd_img.png';
+const String kLogoWhitePath = 'assets/logo/logo_white.png';
+const String kLogoBluePath = 'assets/logo/logo_blue.png';
+
+const kAnimationDuration = Duration(milliseconds: 200);
