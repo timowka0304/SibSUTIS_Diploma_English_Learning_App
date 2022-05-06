513b504dee634c1e2f73c8fe2750564df415d255
timowka0304 <timoti20@mail.ru>
2022-05-06 15:30:12 +0700

    Get data logic in words cards

--- lib/screens/choice_of_word/choice_of_word.dart
+++ lib/screens/choice_of_word/choice_of_word.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import 'package:audioplayers/audioplayers.dart';
 import 'package:easy_peasy/constants.dart';
@@ -10,13 +11,11 @@ import 'package:flip_card/flip_card.dart';
 import 'package:http/http.dart' as http;
 
 class WordsChoice extends StatefulWidget {
-  final List<Word> wordsList;
-  final Map<String, String> headersTranslate;
+  final List<String> wordsList;
 
   const WordsChoice({
     Key? key,
     required this.wordsList,
-    required this.headersTranslate,
   }) : super(key: key);
 
   @override
@@ -42,6 +41,7 @@ class LoadingIndicatorDialog {
     showDialog<void>(
         context: context,
         barrierDismissible: false,
+        barrierColor: kSecondBlue.withOpacity(0.2),
         builder: (BuildContext context) {
           _context = context;
           isDisplayed = true;
@@ -53,7 +53,9 @@ class LoadingIndicatorDialog {
                 children: const [
                   Padding(
                     padding: EdgeInsets.only(left: 16, top: 16, right: 16),
-                    child: CircularProgressIndicator(),
+                    child: CircularProgressIndicator(
+                      color: kMainPink,
+                    ),
                   ),
                 ],
               ),
@@ -71,6 +73,88 @@ class LoadingIndicatorDialog {
 }
 
 class _WordsChoiceState extends State<WordsChoice> {
+  late Future<void> dataFuture;
+
+  var datas = <Word>[];
+
+  int cardIndex = 0;
+  Word word = Word(
+      wordEn: '', wordRu: '', transcription: '', audioFile: '', example: '');
+
+  late Map<String, String> headersTranslate;
+  late List<Word> finalWordsList;
+  late http.Response bearerToken;
+
+  Future<void> getFirstWords(int index) async {
+    //   final results = await Future.wait([
+    //     getWord(widget.wordsList, index).then((value) {
+    //     word = value;
+    //   }),
+    //     getWord(widget.wordsList, index += 1).then((value) {
+    //     word = value;
+    //   }),getWord(widget.wordsList, index += 1).then((value) {
+    //     word = value;
+    //   }),
+    // ]);
+    await getWord(widget.wordsList, index).then((value) {
+      word = value;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    dataFuture = initial();
+  }
+
+  Future<void> initial() async {
+    bearerToken = await getAuthToken();
+    for (int i = 0; i < 1; i++) {
+      await getFirstWords(i);
+      datas.add(word);
+      cardIndex++;
+      print('cardIndex = $cardIndex');
+    }
+    inspect(datas);
+  }
+
+  Future<void> swipe() async {
+    datas.removeAt(0);
+    print('Swipe cardIndex = $cardIndex');
+    await getWord(widget.wordsList, cardIndex).then((value) => word = value);
+    datas.add(word);
+  }
+
+  Future<http.Response> getAuthToken() async {
+    String key =
+        "ZDVlZjZhYWItZGFhZC00ZDlkLWEwOTEtYzY2MWRkYWFlZWU3OjRiZTZjMjhkNDNmZTQ1M2ZiZDZlNTUyZTBiMjhmYmI2";
+    final uri =
+        Uri.parse('https://developers.lingvolive.com/api/v1.1/authenticate');
+    return http.post(
+      uri,
+      headers: {'Authorization': 'Basic ' + key},
+    );
+  }
+
+  Future<Word> getWord(List<String> words, int index) async {
+    headersTranslate = {'Authorization': 'Bearer ' + bearerToken.body};
+    final urlWord = Uri.parse(
+        'https://api.lingvolive.com/Translation/tutor-cards?text=${words[index]}&srcLang=1033&dstLang=1049');
+    final response = await http.get(urlWord, headers: headersTranslate);
+
+    if (response.body != 'null') {
+      List<dynamic> resultWordInfo = await jsonDecode(response.body);
+      return Word.fromJson(resultWordInfo);
+    } else {
+      return Word(
+          wordEn: '',
+          wordRu: '',
+          transcription: '',
+          audioFile: '',
+          example: '');
+    }
+  }
+
   Future<void> playSound(
     String soundName,
     Map<String, String> headersTranslate,
@@ -87,180 +171,241 @@ class _WordsChoiceState extends State<WordsChoice> {
         .body);
 
     final player = AudioCache();
-    await player.playBytes(base64Decode(resultSound));
+    await player.playBytes(
+      base64Decode(
+        resultSound,
+      ),
+    );
 
     LoadingIndicatorDialog().dismiss();
   }
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> cards = List.generate(
-      widget.wordsList.length,
-      (index) => FlipCard(
-        speed: 1000,
-        onFlipDone: (status) {},
-        direction: FlipDirection.HORIZONTAL,
-        front: Card(
-          elevation: 4,
-          shadowColor: kMainTextColor.withOpacity(0.3),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(60),
-          ),
-          color: kWhite,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  widget.wordsList[index].wordEn,
-                  style: TextStyle(
-                      color: kMainTextColor,
-                      fontSize: 26.sp,
-                      fontWeight: FontWeight.w600),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                widget.wordsList[index].audioFile == ''
-                    ? Container()
-                    : GestureDetector(
-                        onTap: () => {
-                          playSound(widget.wordsList[index].audioFile,
-                              widget.headersTranslate, context)
-                        },
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Text(
-                              widget.wordsList[index].transcription,
-                              style: TextStyle(
-                                fontSize: 20.sp,
-                                color: kMainPurple,
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            const Icon(
-                              Icons.volume_up_rounded,
-                              color: kMainPurple,
-                            ),
-                          ],
-                        ),
-                      ),
-              ],
-            ),
-          ),
-        ),
-        back: Card(
-          elevation: 4,
-          shadowColor: kMainTextColor.withOpacity(0.3),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(60),
-          ),
-          color: kWhite,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  widget.wordsList[index].wordRu,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: kMainTextColor,
-                      fontSize: 26.sp,
-                      fontWeight: FontWeight.w600),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  widget.wordsList[index].example,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: kMainTextColor,
-                      fontSize: 20.sp,
-                      fontWeight: FontWeight.w400),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+    // List<Widget> cards = List.generate(
+    //   2,
+    //   (index) => FlipCard(
+    //     speed: 1000,
+    //     onFlipDone: (status) {},
+    //     direction: FlipDirection.HORIZONTAL,
+    //     front: Card(
+    //       elevation: 4,
+    //       shadowColor: kMainTextColor.withOpacity(0.3),
+    //       shape: RoundedRectangleBorder(
+    //         borderRadius: BorderRadius.circular(60),
+    //       ),
+    //       color: kWhite,
+    //       child: Center(
+    //         child: Column(
+    //           mainAxisAlignment: MainAxisAlignment.center,
+    //           children: [
+    //             Text(
+    //               widget.wordsList[index].wordEn,
+    //               style: TextStyle(
+    //                   color: kMainTextColor,
+    //                   fontSize: 26.sp,
+    //                   fontWeight: FontWeight.w600),
+    //             ),
+    //             const SizedBox(
+    //               height: 20,
+    //             ),
+    //             widget.wordsList[index].audioFile == ''
+    //                 ? Container()
+    //                 : GestureDetector(
+    //                     onTap: () => {
+    //                       playSound(widget.wordsList[index].audioFile,
+    //                           widget.headersTranslate, context)
+    //                     },
+    //                     child: Row(
+    //                       mainAxisAlignment: MainAxisAlignment.center,
+    //                       children: <Widget>[
+    //                         Text(
+    //                           widget.wordsList[index].transcription,
+    //                           style: TextStyle(
+    //                             fontSize: 20.sp,
+    //                             color: kMainPurple,
+    //                             fontWeight: FontWeight.w400,
+    //                           ),
+    //                         ),
+    //                         const SizedBox(
+    //                           width: 10,
+    //                         ),
+    //                         const Icon(
+    //                           Icons.volume_up_rounded,
+    //                           color: kMainPurple,
+    //                         ),
+    //                       ],
+    //                     ),
+    //                   ),
+    //           ],
+    //         ),
+    //       ),
+    //     ),
+    //     back: Card(
+    //       elevation: 4,
+    //       shadowColor: kMainTextColor.withOpacity(0.3),
+    //       shape: RoundedRectangleBorder(
+    //         borderRadius: BorderRadius.circular(60),
+    //       ),
+    //       color: kWhite,
+    //       child: Center(
+    //         child: Column(
+    //           mainAxisAlignment: MainAxisAlignment.center,
+    //           children: [
+    //             Text(
+    //               widget.wordsList[index].wordRu,
+    //               textAlign: TextAlign.center,
+    //               style: TextStyle(
+    //                   color: kMainTextColor,
+    //                   fontSize: 26.sp,
+    //                   fontWeight: FontWeight.w600),
+    //             ),
+    //             const SizedBox(
+    //               height: 10,
+    //             ),
+    //             Text(
+    //               widget.wordsList[index].example,
+    //               textAlign: TextAlign.center,
+    //               style: TextStyle(
+    //                   color: kMainTextColor,
+    //                   fontSize: 20.sp,
+    //                   fontWeight: FontWeight.w400),
+    //             ),
+    //           ],
+    //         ),
+    //       ),
+    //     ),
+    //   ),
+    // );
 
-    return Scaffold(
-      backgroundColor: kSecondBlue,
-      body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            physics: const BouncingScrollPhysics(),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TCard(
-                  cards: cards,
-                ),
-                // Text(
-                //   widget.name,
-                //   style: const TextStyle(
-                //     color: kWhite,
-                //     fontWeight: FontWeight.w600,
-                //     fontSize: 20,
-                //   ),
-                // ),
-                // const SizedBox(
-                //   height: 20,
-                // ),
-                // Column(
-                //     children: widget.words.map((word) {
-                //   return Text(
-                //     word,
-                //     style: const TextStyle(
-                //       color: kWhite,
-                //       fontWeight: FontWeight.w400,
-                //       fontSize: 16,
-                //     ),
-                //   );
-                // }).toList()),
-                // const SizedBox(
-                //   height: 80,
-                // ),
-                const SizedBox(
-                  height: 60,
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(
-                      ScreenUtil().setWidth(80),
-                      ScreenUtil().setHeight(40),
-                    ),
-                    maximumSize: Size(
-                      ScreenUtil().setWidth(100),
-                      ScreenUtil().setHeight(40),
-                    ),
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
-                      ),
-                    ),
-                    primary: kWhite,
+    List<Widget> renderCards() {
+      render(Word data, int index) {
+        return SizedBox(
+          height: 250,
+          width: 250,
+          child: Card(
+            elevation: 4,
+            shadowColor: kMainTextColor.withOpacity(0.3),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(60),
+            ),
+            color: kWhite,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(
+                        () {
+                          cardIndex++;
+                          dataFuture = swipe();
+                        },
+                      );
+                      inspect(datas);
+                    },
+                    child: Text("swipe"),
                   ),
-                  onPressed: () => Navigator.pop(context),
-                  child: Text(
-                    'Назад',
+                  Text(
+                    data.wordEn,
                     style: TextStyle(
-                      color: kMainTextColor,
-                      fontSize: 16.sp,
-                    ),
+                        color: kMainTextColor,
+                        fontSize: 26.sp,
+                        fontWeight: FontWeight.w600),
                   ),
-                ),
-              ],
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  data.audioFile == ''
+                      ? Container()
+                      : GestureDetector(
+                          onTap: () => {
+                            playSound(data.audioFile, headersTranslate, context)
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                data.transcription,
+                                style: TextStyle(
+                                  fontSize: 20.sp,
+                                  color: kMainPurple,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              const Icon(
+                                Icons.volume_up_rounded,
+                                color: kMainPurple,
+                              ),
+                            ],
+                          ),
+                        ),
+                ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      }
+
+      return datas.reversed
+          .map((data) => render(data, datas.indexOf(data)))
+          .toList();
+    }
+
+    return FutureBuilder(
+      future: dataFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            backgroundColor: kSecondBlue,
+            body: SafeArea(
+              child: Center(
+                child: CircularProgressIndicator(
+                  color: kMainPink,
+                ),
+              ),
+            ),
+          );
+        } else {
+          return Scaffold(
+            backgroundColor: kSecondBlue,
+            body: SafeArea(
+              child: Center(
+                child: Stack(
+                  children: renderCards(),
+                ),
+                // child: TCard(
+                //   cards: renderCards(),
+                //   controller: _controller,
+                //   onForward: (_, info) {
+                //     if (info.direction == SwipDirection.Right) {
+                //       print('like');
+                //       setState(() {
+                //         cardIndex++;
+                //         dataFuture = swipe();
+                //       });
+                //       inspect(datas);
+                //     } else {
+                //       print('dislike');
+                //       setState(() {
+                //         cardIndex++;
+                //         dataFuture = swipe();
+                //       });
+                //       inspect(datas);
+                //     }
+                //   },
+                // ),
+              ),
+              // child: Stack(
+              //   children: renderCards(),
+              // ),
+            ),
+          );
+        }
+      },
     );
   }
 }
